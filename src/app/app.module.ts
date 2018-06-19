7722cdced6f73f1b5e53b38459381b7d75398bc3
Notalama <dkoblenc@gmail.com>
2018-06-19 11:11:27 +0300

    sass + material theme implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
